a5ec65add3837734b210de1004fa706c0d6d37b4
Daniel Ortega <danort.97@gmail.com>
2025-10-23 00:54:40 -0700

    add hashing sample

---
+++ 8.Auth/Samples/Hashing/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+namespace Hashing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a password: ");
+            string? password = Console.ReadLine();
+
+
+            byte[] salt;
+            Console.Write("Use salt? (y/n): ");
+            string? useSaltInput = Console.ReadLine();
+
+            if (useSaltInput?.ToLower() != "y")
+            {
+                salt = [];
+            }
+            else
+            {
+                // Generate a 128-bit salt using a sequence of
+                // cryptographically strong random bytes.
+                salt = RandomNumberGenerator.GetBytes(128 / 8);
+                Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+            }
+
+            // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password!,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+
+            Console.WriteLine($"Hashed password: {hashed}");
+        }
+    }
+}
